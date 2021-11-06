0adcbc47f321877c29d4082440b858c979b23153
Keworker <84124869+Keworker@users.noreply.github.com>
2021-11-06 17:10:56 +0500

    Update Main.java

--- app/src/main/java/com/samsung/moduletwo/Main.java
+++ app/src/main/java/com/samsung/moduletwo/Main.java
@@ -11,48 +11,34 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-public class Main extends AppCompatActivity implements View.OnClickListener {
+public class Main extends AppCompatActivity {
 
-TextView text;
-SeekBar sp;
+TextView text; //Сообщение о результате
+SeekBar sp; //Отображение ползунка в процентах для пользователя
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        Integer itog = 2;
-        if (MainActivity.retres().equals("инкапсуляция") || MainActivity.retres().equals("Инкапсуляция")) {
-            itog++;
-            Log.d("qwerty23", itog.toString());
+        int itog = 2; //Переменная результата
+        if (MainActivity.res.equals("инкапсуляция") || MainActivity.res.equals("Инкапсуляция")) {
+            itog++; //Если ответ правильный, добавляем 1 балл
         }
-        itog -= MainActivity2.returner()[0];
-        Log.d("qwerty23", itog.toString());
-        itog += MainActivity2.returner()[1];
-        Log.d("qwerty23", itog.toString());
-        itog -= MainActivity2.returner()[2];
-        Log.d("qwerty23", itog.toString());
-        itog += MainActivity2.returner()[3];
-        Log.d("qwerty23", itog.toString());
-        itog += MainActivity3.returner();
-        Log.d("qwerty23", itog.toString());
-        if (itog < 0) {
-            itog = 0;
-        }
-        int viewing = 100 * itog / 6;
-        text = findViewById(R.id.text);
-        text.setText(("Вы набрали " + itog + " из 6 баллов"));
-        sp = findViewById(R.id.sp);
-        sp.setProgress(viewing);
-        sp.setOnTouchListener(new View.OnTouchListener(){
-            @Override
+        itog -= MainActivity2.res[0]; //Этот вариант не правильный, если он нажат - балл
+        itog += MainActivity2.res[1]; //Этот вариант правильный, если он нажат + балл
+        itog -= MainActivity2.res[2]; //Этот вариант не верный, если он нажат - балл
+        itog += MainActivity2.res[3]; //Этот вариант верный, если он нажат + балл
+        itog += MainActivity3.res; //Прибавляем баллы (0-1) за последний вопрос
+        int viewing = 100 * itog / 6; //Переменная для представления итога в процентах
+        text = findViewById(R.id.text); //Находим текст
+        text.setText(("Вы набрали " + itog + " из 6 баллов")); //Пишем результат текстом
+        sp = findViewById(R.id.sp); //Находим сикбар
+        sp.setProgress(viewing); //Заполняем его на необходимое число процентов
+        sp.setOnTouchListener(new View.OnTouchListener(){ //Перезаписываем метод прикосновения к сикбару,
+            @Override //Что бы пользователь не мог поменять его положение
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 return true;
             }
         });
-    }
-
-    @Override
-    public void onClick(View view) {
-
     }
 }
